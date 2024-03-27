d17945f56652b163a2a2fad69947a8b4109c3370
arka1997 <debanjansarkar9981@gmail.com>
2024-03-27 18:49:24 +0530

    Modified Todo

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Homepage from './homepage/Homepage';
 // import Header from './components/Header.js'
 // import Middlelayer from './components/Middlelayer.js'
 // import Footer from './components/Footer.js'
-// import Todo from './Todo';
+import Todo from './Todo';
 function App() {
   return (
     <div className="App">
-      {/* <Todo/> */}
+      <Todo/>
       <Homepage/>
     </div>
   );
