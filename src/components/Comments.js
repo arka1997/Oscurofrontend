9aa31820451b601982c60619599a45ebec75326b
arka1997 <debanjansarkar9981@gmail.com>
2024-07-23 23:25:19 +0530

    Like Button of Root Comment Active Inactive

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,55 +1,107 @@
-import React, { useState } from 'react'
-import "./Comments.css"
-import {FaHeart} from "react-icons/fa"
-import { FaReply } from "react-icons/fa"
-import {FaEdit} from "react-icons/fa"
-import {FaTrash} from "react-icons/fa"
-import { IconBtn } from './IconBtn'
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import "./Comments.css";
+import { FaEdit, FaHeart, FaRegHeart, FaReply, FaTrash } from "react-icons/fa";
+import { IconBtn } from "./IconBtn";
 
-
-const Comments = ({ id, userName, rootMessage, timestamp }) => {
+const Comments = ({ _id, postId, rootMessage, userName, rootCommentLikes, timestamp }) => {
   // const {getReplies} = "dfdf"
   // const childComments = getReplies(id) // here we pass the id of the current root comment to get its child comments
-  const [areChildrenHidden, setAreChildrenHidden] = useState(false)
+
+  const [isLiked, setIsLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(rootCommentLikes);
+  const [areChildrenHidden, setAreChildrenHidden] = useState(false);
   const currentDate = new Date(timestamp);
   const currentDateISOString = currentDate.toISOString();
+  const [comments, setComments] = useState([]);
 
+  // useEffect(() => {
+  //   const fetchRootComments = async () => {
+  //     try {
+  //     for(const postsId of postId){
+        
+  //     }
+  //       const response = await axios.get("http://localhost:4000/fetchRootComments", {
+  //         params: {
+  //           postId: postId // Add your parameter here
+  //         }
+  //       });
+  //       response.data.map((data) => {
+  //         console.log(data)
+  //       })
+  //       await setComments(response.data[0]);
+  //     } catch (error) {
+  //       console.error('Error fetching root comments:', error);
+  //     }
+  //   };
+
+  //   fetchRootComments();
+  // }, [_id]);
+
+  // const addChildCommentLikes = async (rootCommentId, rootCommentLikes, isLiked) => {
+  //   try {
+  //     const updatedLikesCount = isLiked ? likesCount - 1 : likesCount + 1;
+  //     const response = await axios.post(`http://localhost:4000/updateRootCommentLikes/${_id}/${updatedLikesCount}`);
+            
+  //         console.log(response.data)
+  //     const updatedComments = comments.map(comment =>
+  //       comment._id === rootCommentId
+  //         ? { ...comment, isLiked: response.data.isLiked, rootCommentLikes: response.data.likes }
+  //         : comment
+  //     );
+  //     setComments(updatedComments);
+  //     setLikesCount(updatedLikesCount);
+  //     setIsLiked(!isLiked);
+  //   } catch (error) {
+  //     console.error('Error updating root comment likes:', error);
+  //   }
+  // }
+  
+
+
+  const addChildCommentLikes = async () => {
+    try {
+      const likeStatus = isLiked ? 0 : 1;// depending on boolean value "isLiked", we set to either 0 or 1
+      await axios.post(`http://localhost:4000/updateRootCommentLikes/${_id}/${likeStatus}`);
+      setLikesCount((prevLikes) => prevLikes + (isLiked ? -1 : 1));
+      setIsLiked(!isLiked);
+    } catch (error) {
+      console.error('Error updating comment likes:', error);
+    }
+  };
   return (
     <>
       {/* This first one is the Parent comment with header footer, and after that, we print its child comments */}
       {/* Create Comment Header */}
+
       <div className="comment">
-          <div className="header">
-            {/* Also add user IMAGE along with name in comments */}
-            <span className="name">{userName}</span>
-            <span className="id">{id}</span>
-            <span className="date">{currentDateISOString}</span>
-          </div>
-          {/* Create Comment Message Body */}
-          <div className="message">{rootMessage}</div>
-          
-          {/* Create Comment Footer */}
-          <div className="footer">
-            <IconBtn Icon={FaHeart}>
-              2
-            </IconBtn>
-            <IconBtn Icon={FaReply}>
-              2
-            </IconBtn>
-            <IconBtn Icon={FaEdit}>
-              2
-            </IconBtn>
-            <IconBtn Icon={FaTrash} colour="red">
-              2
-            </IconBtn>
-          </div>
+        <div className="header">
+          {/* Also add user IMAGE along with name in comments */}
+          <span className="name">{userName}</span>
+          <span className="id">{_id}</span>
+          <span className="date">{currentDateISOString}</span>
+        </div>
+
+        {/* Create Comment Message Body */}
+        <div className="message">{rootMessage}</div>
+
+        {/* Create Comment Footer */}
+        <div className="footer">
+        <IconBtn Icon={isLiked ? FaHeart : FaRegHeart} onClick={addChildCommentLikes}>
+          {likesCount}
+        </IconBtn>
+          <IconBtn Icon={FaReply}>2</IconBtn>
+          <IconBtn Icon={FaEdit}></IconBtn>
+          <IconBtn Icon={FaTrash} colour="red">
+            2
+          </IconBtn>
+        </div>
       </div>
-     
     </>
-    
+
     /* Now Comment Form will be present in 2 places, one is just below post when I click Comment Btn.
     The other time is when we want to update the Comment(Parent or Child) */
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
--- src/timeline/posts/Post.js
+++ src/timeline/posts/Post.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Post.css";
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
-import Tooltip from '@material-ui/core/Tooltip';
-import { useSelector } from 'react-redux';
-import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
-import CommentForm from '../../components/CommentForm';
-import CommentList from '../../components/homepage/CommentList';
+import Tooltip from "@material-ui/core/Tooltip";
+import { useSelector } from "react-redux";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import CommentForm from "../../components/CommentForm";
+import CommentList from "../../components/homepage/CommentList";
 
 const Post = ({ post, rootComments }) => {
-  const posts = useSelector(state => state.posts);
+  const posts = useSelector((state) => state.posts);
   const [showUpdateMessage, setShowUpdateMessage] = useState("");
   const [showDeletePopup, setShowDeletePopup] = useState(false);
   const [showComments, setShowComments] = useState([]);
@@ -22,7 +22,7 @@ const Post = ({ post, rootComments }) => {
   const [activePostId, setActivePostId] = useState(null);
 
   const getRandomColor = () => {
-    const colorCode = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    const colorCode = "#" + Math.floor(Math.random() * 16777215).toString(16);
     return colorCode;
   };
 
@@ -31,12 +31,14 @@ const Post = ({ post, rootComments }) => {
       try {
         const likesData = {};
         for (const post of posts[0].text) {
-          const response = await axios.get(`http://localhost:4000/getLikes/${post._id}`);
-          likesData[post._id] = (response.data.postLikes !== 0);
+          const response = await axios.get(
+            `http://localhost:4000/getLikes/${post._id}`
+          );
+          likesData[post._id] = response.data.postLikes !== 0;
         }
         setLikes(likesData);
       } catch (error) {
-        console.error('Error fetching initial likes:', error);
+        console.error("Error fetching initial likes:", error);
       }
     };
     fetchInitialLikes();
@@ -44,94 +46,115 @@ const Post = ({ post, rootComments }) => {
 
   const addLike = async (postId) => {
     try {
-      setLikes(prevLikes => ({
-        ...prevLikes,
-        [postId]: !prevLikes[postId]
+      
+      setLikes((prevLikes) => ({
+        ...prevLikes, // keeps a copy of the previous likes
+        [postId]: !prevLikes[postId], // Matches the new updated data with the old copy
       }));
       await updateLikes(postId, !likes[postId] ? 1 : 0);
     } catch (error) {
-      console.error('Error adding like:', error);
+      console.error("Error adding like:", error);
     }
   };
 
   const updateLikes = async (postId, likeStatus) => {
     try {
-      await axios.post(`http://localhost:4000/updateLikes/${postId}/${likeStatus}`);
-      (likeStatus === 1) ? setShowUpdateMessage("Liked Successfully") : setShowUpdateMessage("");
+      await axios.post(
+        `http://localhost:4000/updateLikes/${postId}/${likeStatus}`
+      );
+      likeStatus === 1
+        ? setShowUpdateMessage("Liked Successfully")
+        : setShowUpdateMessage("");
     } catch (error) {
-      console.error('Error updating likes:', error);
+      console.error("Error updating likes:", error);
     }
   };
 
   const fetchRootComments = async (postId) => {
-    console.log(postId)
-    setActivePostId(prevPostId => prevPostId === postId ? null : postId);
+    console.log(postId);
+    setActivePostId((prevPostId) => (prevPostId === postId ? null : postId));
 
     try {
-      const response = await axios.get(`http://localhost:4000/getPostRootComments/${postId}`);
+      const response = await axios.get(
+        `http://localhost:4000/getPostRootComments/${postId}`
+      );
       const rootCommentList = response.data.postComments || [];
       setShowComments(rootCommentList);
-      console.log(rootCommentList);
+      console.log(rootCommentList[0]._id);
     } catch (error) {
-      console.error('Error fetching comments:', error);
+      console.error("Error fetching comments:", error);
       setShowComments([]); // Fallback to empty array in case of error
     }
   };
 
-  
-
   return (
     <>
-      {showUpdateMessage === "Liked Successfully" && <div className='response-card'>{showUpdateMessage}</div>}
+      {showUpdateMessage === "Liked Successfully" && (
+        <div className="response-card">{showUpdateMessage}</div>
+      )}
       {posts?.map((post) => (
-        <div className='post' key={post.id}>
+        <div className="post" key={post.id}>
           {post.text?.map((item, index) => (
             <div key={`${item.id}-${index}`}>
-              <div className='post__header'>
-                <div className='post__headerAuthor'>
-                  <Avatar style={{ marginRight: "10px" }} sx={{ bgcolor: getRandomColor() }}>
-                  </Avatar>{" "}
+              <div className="post__header">
+                <div className="post__headerAuthor">
+                  <Avatar
+                    style={{ marginRight: "10px" }}
+                    sx={{ bgcolor: getRandomColor() }}
+                  ></Avatar>{" "}
                   {item.userName} • <span>{item.timestamp}</span>
                 </div>
                 <Tooltip title="Delete Post">
-                  <div className='moreOptionsIcon' onClick={() => setShowDeletePopup(true)}>
+                  <div
+                    className="moreOptionsIcon"
+                    onClick={() => setShowDeletePopup(true)}
+                  >
                     <MoreHorizIcon />
                   </div>
                 </Tooltip>
               </div>
-              <div className='post__image'>
+              <div className="post__image">
                 <img
                   src={`http://localhost:4000/${item.postImage}`} // Dynamic image source
                   alt="Post"
                 />
               </div>
-              <div className='post__footer'>
-                <div className='post__footerIcons'>
-                  <div className='post__iconsMain'>
+              <div className="post__footer">
+                <div className="post__footerIcons">
+                  <div className="post__iconsMain">
                     {likes[item._id] ? (
-                      <FavoriteIcon className="fas postIcon" onClick={() => addLike(item._id)} />
+                      <FavoriteIcon
+                        className="fas postIcon"
+                        onClick={() => addLike(item._id)}
+                      />
                     ) : (
-                      <FavoriteBorderIcon className="postIcon" onClick={() => addLike(item._id)} />
+                      <FavoriteBorderIcon
+                        className="postIcon"
+                        onClick={() => addLike(item._id)}
+                      />
                     )}
-                    <ChatOutlinedIcon className="postIcon" onClick={() => fetchRootComments(item._id)} />
-                    <TelegramIcon className="postIcon" />
+                      <ChatOutlinedIcon
+                        className="postIcon"
+                        onClick={() => fetchRootComments(item._id)}
+                      />
+                      <TelegramIcon className="postIcon" />
                   </div>
-                  <div className='post__iconsSave'>
-                    <BookmarkBorderIcon className="postIcon" />
+                  <div className="post__iconsSave">
+                      <BookmarkBorderIcon className="postIcon" />
                   </div>
                 </div>
                 Liked by {item.postLikes} people.
               </div>
-              {activePostId === item._id && 
+              {activePostId === item._id && (
                 <>
-                  <CommentForm 
-                    postId={item._id}
-                    userName={item.userName}
-                    timestamp={item.timestamp}
-                  />
-                  <CommentList comments={showComments} />
-              </>
-            }
+                      <CommentForm
+                        postId={item._id}
+                        userName={item.userName}
+                        timestamp={item.timestamp}
+                      />
+                      <CommentList comments={showComments} />
+                </>
+              )}
             </div>
           ))}
         </div>
@@ -142,5 +165,5 @@ const Post = ({ post, rootComments }) => {
 
 export default Post;
 
-// FaFirstAid, the api is called to get the Parent Comments List -> then in the posts page -> onclick of comment button -> we display form and send the root comments that was fetched from Db, as a Parameter to CommentList page.
+// First, the api is called to get the Parent Comments List -> then in the posts page -> onclick of comment button -> we display form and send the root comments that was fetched from Db, as a Parameter to CommentList page.
 // CommentList Page has the component Comment. We inject the COMMENTS inside that COMMENTS ONE BY ONE THROUGH TRAVERSING
